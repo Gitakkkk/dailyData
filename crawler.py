67b2ee38500ed857fb4edad4d6bb2c634e8c74ba
Gitakkkk <stom1028@naver.com>
2022-12-17 03:15:39 +0900

    add: 제목 & 링크 크롤링

--- crawler.py
+++ crawler.py
@@ -7,22 +7,32 @@ import re
 import time
 import dotenv
 import os
+import datetime
 
 dotenv_file = dotenv.find_dotenv()
 dotenv.load_dotenv(dotenv_file)
 
 options = webdriver.ChromeOptions()
-# options.add_argument('headless')
+options.add_argument('headless')
 options.add_argument('window-size=1920x1080')
 options.add_argument("disable-gpu")
 options.add_argument(r'/Users/nagitak/Library/Application Support/Google/Chrome/Profile 3')
 options.add_experimental_option("detach", True)
 
-def no_space(text):
+# html element 중 필요 없는 개행과 공백 제거
+def noSpace(text):
     text1 = re.sub('&nbsp; | &nbsp;| \n|\t|\r', '', text)
     reuslt = re.sub('\n\n', '', text1)
     return reuslt
 
+# 채널 필터링
+def textMatch(text):
+    if re.search(r'매일경제', text):
+        return text
+    else:
+        return text
+        
+# browser 열고 로그인 후 news url 진입
 browser = webdriver.Chrome(executable_path='/Users/nagitak/Desktop/dailyNews/chromedriver', chrome_options=options)
 browser.get('https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com')
 time.sleep(3)
@@ -38,9 +48,16 @@ browser.find_element(By.ID, 'log.login').click()
 time.sleep(3)
 browser.get('https://news.naver.com')
 
+soup = BeautifulSoup(browser.page_source, 'lxml') # html parsing
 
-
-# soup = BeautifulSoup(browser.page_source, 'lxml') # html parsing
-
-# 상위 3개 타이틀 클릭하고 타이틀 & url 복사해서 DB에 저장
-
+# title & url 크롤링해서 DB에 저장
+news = soup.find_all('div', attrs={'class': 'main_brick_item _channel_news_card_wrapper'})
+for i in range(len(news)):
+    title_first =  news[i].find_all('a', attrs={'class': 'cc_text_a _need_nclick _cds_link'})[0].text
+    title_second =  news[i].find_all('a', attrs={'class': 'cc_text_a _need_nclick _cds_link'})[1].text
+    title_third =  news[i].find_all('a', attrs={'class': 'cc_text_a _need_nclick _cds_link'})[2].text
+    link_first =  news[i].find_all('a', attrs={'class': 'cc_text_a _need_nclick _cds_link'})[0]['href']
+    link_second =  news[i].find_all('a', attrs={'class': 'cc_text_a _need_nclick _cds_link'})[1]['href']
+    link_third =  news[i].find_all('a', attrs={'class': 'cc_text_a _need_nclick _cds_link'})[2]['href']
+    date = datetime.datetime.now().strftime('%y-%m-%d %H:%M')
+    print(title_first, link_first, title_second, link_second, title_third, link_third, date)
