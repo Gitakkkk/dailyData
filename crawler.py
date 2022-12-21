07aeb5be14460f99cd43cff4769c2e667f4ad6a5
Gitakkkk <stom1028@naver.com>
2022-12-21 18:56:46 +0900

    fix: githubAction

--- crawler.py
+++ crawler.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-from github import Github, Issue
+# from github import Github, Issue
 import pyperclip
 import re
 import time
